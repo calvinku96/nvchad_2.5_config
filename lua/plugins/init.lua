8e9e15d4005b1d2d6320198bf3421b3c008deeea
Calvin <calvinku96@gmail.com>
2025-10-23 09:56:40 +0800

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -315,4 +315,12 @@ return {
       }
     end,
   },
+  {
+    "benomahony/uv.nvim",
+    ft = "python",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = {
+      picker_integration = true,
+    },
+  },
 }
